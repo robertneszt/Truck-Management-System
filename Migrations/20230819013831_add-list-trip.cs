eb1aa66c96576ec947e62d188f086c2660d8ff61
Weiguang0406 <weiguangy2022@gmail.com>
2023-08-19 10:58:03 -0400

    added List trip to the drivetripviewModel

---
+++ Migrations/20230819013831_add-list-trip.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TMS_APP.Migrations
+{
+    /// <inheritdoc />
+    public partial class addlisttrip : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
